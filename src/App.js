38a55e5808bffc6eeeb101c4cd5b61f7845f1692
iTomsic <patriktomsic2@gmail.com>
2025-11-21 12:42:23 +0100

    End 1.KV

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
 
 function App() {
   return (
-    <div>
+    
+    
+    <Router>
+
       <Navbar />
       <main style={{ padding: "20px" }}>
-      <ProductList />
-      <Cart />
+
+
+      <Routes>
+      <Route path="/products" element={<ProductList />} />
+      <Route path="/cart" element={<Cart />} />
+      </Routes>
+
       </main>
-    </div>
+
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return(
         <nav style={{
             background: "black",
             color: "white",
             padding: "10px 20px",
-
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
         }}>
     
-        <h2>My Webshop</h2>
+            <h2 style={{ margin: 0}}>My Webshop</h2>
+
+            <div syle={{display: "flex", gap:"20px"}}>
+
+                <Link to="/products" style={{color: "white", textDecoration: "none"}}>
+                Products ||  
+                </Link>
+
+                <Link to="/cart" style={{color: "white", textDecoration: "none"}}>
+                Cart  
+                </Link>
+
+            </div>
+
+
         </nav>
 
     );
