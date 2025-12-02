1244ac15f99a6be13b925c298897434509f92c9f
iTomsic <patriktomsic2@gmail.com>
2025-12-02 19:30:43 +0100

    Add Product, User and Order models

---
+++ webshop-backend/models/Order.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    items:[
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    total: Number,
+    status: {
+        type: String,
+        default: pending
+    }
+    
+}, { timestamps: true });
+
+export default mongoose.model("Order", orderSchema);
---
+++ webshop-backend/models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+
+    title: { type: String, required: true },
+    author: String,
+    description: String,
+    price: { type: Number, required: true },
+    image: String,
+    category: String,
+    stock: { type: Number, default: 0 }
+    
+}, { timestamps: true });
+
+export default mongoose.model("Product", productSchema);
